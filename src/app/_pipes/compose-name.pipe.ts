bbc5694fd16e78a13db59bc8d028e30c4a3e8db6
Benjamin Zvolenszki <zvolenszki.benjamin@gmail.com>
2025-04-18 18:19:19 +0300

    feat: add length validation to pipe [3/A/4]

--- src/app/_pipes/compose-name.pipe.ts
+++ src/app/_pipes/compose-name.pipe.ts
@@ -7,6 +7,10 @@ import {Name} from "../_interfaces/name";
 export class ComposeNamePipe implements PipeTransform {
 
   transform(value: Name, lang: 'hu' | 'en' = 'hu'): string {
+    if (value.firstName.length < 3 || value.lastName.length < 3) {
+      return 'Nem megfelelő név!';
+    }
+
     return (lang === 'hu'
       ? [value.title, value.lastName, value.middleName, value.firstName]
       : [value.title, value.firstName, value.middleName, value.lastName])
